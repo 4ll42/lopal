9dfefa8a189d263bf728beb26aa0158312721c14
4ll42 <jaque061523@gmail.com>
2026-04-08 08:46:22 -0300

    Arrays, Conjunto, Mapa,Callback Function e closure

---
+++ array/array.js
@@ -0,0 +1,15 @@
+function arranjo(){
+    let turma = ["alessandra", "Alexandre", "Allan", "Ana"];
+    console.log(turma);
+    console.log(turma[0]);
+
+    const frutas = [];
+    frutas[0] = "banana"
+    frutas[1] = "laranja"
+    frutas[2] = "maça"
+    
+    console.log(frutas);
+
+    for( i = 0; i < frutas.length; i++){
+        console.log(i + " " + frutas[i]);
+    } 
